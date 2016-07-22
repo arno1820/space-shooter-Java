91474759eee01ea3374c43aa6d414983c6d61b2e
arno1820 <arnorondou@hotmail.com>
2016-07-22 21:35:06 +0200

    bullet manager

---
+++ The Game/src/project_Beta/BulletManager.java
@@ -0,0 +1,13 @@
+package project_Beta;
+
+import java.util.ArrayList;
+
+import engine.core.gfx.Image;
+
+public class BulletManager {
+
+	private ArrayList<Integer> bullettype;
+	// bullet A on position b has vector on location 2b
+	private ArrayList<Double> vectorList;
+	
+}
